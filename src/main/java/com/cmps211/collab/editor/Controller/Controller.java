0a4b3ad9127f6824a0d9fb2116cc9f1c242882e5
Marwan <Marwan.Ibrahim04@eng-st.cu.edu.eg>
2024-05-05 23:27:49 +0200

    log in updated

--- src/main/java/com/cmps211/collab/editor/Controller/Controller.java
+++ src/main/java/com/cmps211/collab/editor/Controller/Controller.java
@@ -1,7 +1,10 @@
 package com.cmps211.collab.editor.Controller;
 
 import com.cmps211.collab.editor.Service.*;
+import com.cmps211.collab.editor.Model.*;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +25,10 @@ public class Controller {
     }
 
     @PostMapping("/login")
-    public String logIn(@RequestBody String userInfo) {
-        String[] userInformation = userInfo.split(":");
-        return userService.logIn(userInformation[0], userInformation[1]);
+    public ResponseEntity<User> logIn(@RequestBody User userInfo) {
+        if (userService.logIn(userInfo)) {
+            return ResponseEntity.ok().body(userInfo);
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
