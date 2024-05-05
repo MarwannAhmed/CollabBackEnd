f8b11cd96cfb04c779a4bb9f7c71be2b9ed2b942
Marwan <Marwan.Ibrahim04@eng-st.cu.edu.eg>
2024-05-06 00:01:14 +0200

    log in updated

--- src/main/java/com/cmps211/collab/editor/Configuration/CorsConfig.java
+++ src/main/java/com/cmps211/collab/editor/Configuration/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.cmps211.collab.editor.Configuration;
 
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,14 +11,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow CORS for all endpoints
-                .allowedOrigins("https://collaborativeeditor.vercel.app") // Allow requests from the specified origin
-                .allowedMethods("*") // Allow all HTTP methods
-                .allowedHeaders("*"); // Allow all headers
-
-        // If you also want to allow requests from any subpath of the specified origin:
-        registry.addMapping("/**") // Allow CORS for all endpoints
-                .allowedOrigins("https://collaborativeeditor.vercel.app/*") // Allow requests from the specified origin and its subpaths
-                .allowedMethods("*") // Allow all HTTP methods
+                .allowedOrigins("https://collaborativeeditor.vercel.app") // Replace with your client's URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Add allowed methods as required
                 .allowedHeaders("*"); // Allow all headers
     }
 }
