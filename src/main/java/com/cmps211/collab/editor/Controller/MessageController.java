e6d463b2de8e16fc929dcc5f4ef6c389c8b6e74a
Marwan <Marwan.Ibrahim04@eng-st.cu.edu.eg>
2024-05-15 04:41:16 +0200

    test

--- src/main/java/com/cmps211/collab/editor/Controller/MessageController.java
+++ src/main/java/com/cmps211/collab/editor/Controller/MessageController.java
@@ -6,54 +6,42 @@ import com.cmps211.collab.editor.Service.DocService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-// import org.springframework.messaging.handler.annotation.Payload;
-// import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 @Controller
 public class MessageController {
 
-    private final DocService docService;
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
 
     @Autowired
-    SimpMessagingTemplate simpMessagingTemplate;
-    private BlockingQueue<Message> messageQueue = new LinkedBlockingQueue<>(1);
-    // private Object semaphore;
-    // private boolean free = true;
+    private DocService docService;
 
-    MessageController(DocService ds) {
-        // semaphore = new Object();
-        docService = ds;
-        // free = true;
-    }
+    private final Object mutex = new Object();
+    private int lock = 0;
 
     @MessageMapping("/application/{docID}")
-    public void send(final Message message, @DestinationVariable String docID) throws Exception {
-        // synchronized (semaphore) {
-        // System.out.println(free);
-        // if (free == true) {
-        // free = false;
-        // } else {
-        // return;
-        // }
-        // }
-        if (!messageQueue.offer(message)) {
-            return;
+    public void passMessage(final Message message, @DestinationVariable String docID) {
+        synchronized (mutex) {
+            if (lock == 0) {
+                lock = 1;
+            } else {
+                return;
+            }
         }
-        simpMessagingTemplate.convertAndSend("/all/messages/" + docID, message);
+        messagingTemplate.convertAndSend("/all/messages/" + docID, message);
         docService.changeContent(message, docID);
-        messageQueue.poll();
-        // synchronized (semaphore) {
-        // free = true;
-        // }
-    }
+        synchronized (mutex) {
+            lock = 0;
+        }
 
-    // @MessageMapping("/private")
-    // public void sendToSpecificUser(@Payload Message message) {
-    // simpMessagingTemplate.convertAndSendToUser(message.getTo(), "/specific",
-    // message);
-    // }
+    }
 }
+
+// @MessageMapping("/private")
+// public void sendToSpecificUser(@Payload Message message) {
+// simpMessagingTemplate.convertAndSendToUser(message.getTo(), "/specific",
+// message);
+// }
+// }
